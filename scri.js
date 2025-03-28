c4c90061b4a3b833b760596acd8f807a327d4ccd
Isra Guitar <israelflores408@gmail.com>
2025-03-28 13:30:04 -0500

    add tarea

--- scri.js
+++ scri.js
@@ -1,12 +1,36 @@
-const $ = (el) => document.querySelector(el)
-const $$ = (el) => document.querySelectorAll(el)
+const $ = (el) => document.querySelector(el);
+const $$ = (el) => document.querySelectorAll(el);
 
-const addTareaButton = $('#add-tarea ')
-const modal = $('#modal-add-tarea')
-const modal = $('#modal-add-tarea')
+//const addTareaButton = $('#add-tarea')
+//const modal = $('#modal-add-tarea')
+//const closeModalAddTarea = $('#closeModalBtn')
+const addTareaButton = document.getElementById("add-tarea");
+const modal = document.getElementById("modal-add-tarea");
+const closeModalAddTarea = document.getElementById("closeModalBtn");
 
+function abrirModal(modalTemp) {
+  modalTemp.style.display = "flex";
+  setTimeout(() => {
+    modalTemp.classList.add("show");
+  }, 50);
+}
 
-addTareaButton.addEventListener('click', () => {
-    console.log('Hola')
-})
+function cerrarModal(modalTemp) {
+  modalTemp.classList.remove("show");
+  setTimeout(() => {
+    modalTemp.style.display = "none";
+  }, 300);
+}
 
+addTareaButton.addEventListener("click", () => {
+  abrirModal(modal);
+});
+closeModalAddTarea.addEventListener("click", () => {
+  cerrarModal(modal);
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target == modal) {
+    cerrarModal(modal);
+  }
+});
