bbef530ed3e37d73367dd58d6cc397f09ac82788
Israel Flores <israelflores408@gmail.com>
2025-03-28 00:27:30 -0500

    creando modal

---
+++ scri.js
@@ -0,0 +1,12 @@
+const $ = (el) => document.querySelector(el)
+const $$ = (el) => document.querySelectorAll(el)
+
+const addTareaButton = $('#add-tarea ')
+const modal = $('#modal-add-tarea')
+const modal = $('#modal-add-tarea')
+
+
+addTareaButton.addEventListener('click', () => {
+    console.log('Hola')
+})
+
