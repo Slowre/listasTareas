90c3c133c11275bb8c435658582227db5ad5c768
Isra Guitar <israelflores408@gmail.com>
2025-04-01 11:21:23 -0500

    Estilo form add lista terminado

--- scri.js
+++ scri.js
@@ -1,32 +1,55 @@
 const $ = (el) => document.querySelector(el);
 const $$ = (el) => document.querySelectorAll(el);
 
-
 const addTareaButton = document.getElementById("add-tarea");
-const modal = document.getElementById("modal-add-tarea");
-const closeModalAddTarea = document.getElementById("closeModalBtn");
 const containformAddList = document.getElementById("contain-form-add-list");
+const closeformAddListButton = document.getElementById("close-form-add-list");
+const formAddList = document.getElementById("form-add-list");
 
-function openModal(modalTemp) {
-  modalTemp.style.display = "flex";
-  setTimeout(() => {
-    modalTemp.classList.add("show");
-  }, 50);
+let isFormAddListVisible = false;
+const DISPLAY_FLEX = "flex";
+const DISPLAY_NONE = "none";
+
+function changeFormAddlistVisibility() {
+  if (isFormAddListVisible) {
+    containformAddList.style.display = DISPLAY_NONE;
+    addTareaButton.style.display = DISPLAY_FLEX;
+    formAddList.reset();
+  } else {
+    addTareaButton.style.display = DISPLAY_NONE;
+    containformAddList.style.display = DISPLAY_FLEX;
+  }
+  isFormAddListVisible = !isFormAddListVisible;
 }
 
-function closeModal(modalTemp) {
-  modalTemp.classList.remove("show");
-  setTimeout(() => {
-    modalTemp.style.display = "none";
-  }, 300);
-}
+addTareaButton.addEventListener("click", changeFormAddlistVisibility);
 
-addTareaButton.addEventListener("click", () => {
-  addTareaButton.style.display = "none";
-  containformAddList.style.display = "flex";
+closeformAddListButton.addEventListener("click", changeFormAddlistVisibility);
+
+window.addEventListener("click", (event) => {
+  if (
+    event.target != addTareaButton &&
+    !event.target.closest("#contain-form-add-list")
+  ) {
+    if (isFormAddListVisible) {
+      changeFormAddlistVisibility();
+    }
+  }
 });
 
-containformAddList.addEventListener("click", () => {
-  containformAddList.style.display = "none"; 
-    addTareaButton.style.display = "flex"; 
-});
+// const modal = document.getElementById("modal-add-tarea");
+// const closeModalAddTarea = document.getElementById("closeModalBtn");
+
+// function openModal(modalTemp) {
+//   modalTemp.style.display = "flex";
+//   setTimeout(() => {
+//     modalTemp.classList.add("show");
+//   }, 50);
+// }
+
+// function closeModal(modalTemp) {
+//   modalTemp.classList.remove("show");
+//   setTimeout(() => {
+//     modalTemp.style.display = "none";
+//   }, 300);
+// }
