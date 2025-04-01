5666940b611ad1b79ccb0b8b628206d58e82b6e7
Isra Guitar <israelflores408@gmail.com>
2025-04-01 14:21:02 -0500

    Añadir nueva lista visualmente

--- scri.js
+++ scri.js
@@ -5,6 +5,8 @@ const addTareaButton = document.getElementById("add-tarea");
 const containformAddList = document.getElementById("contain-form-add-list");
 const closeformAddListButton = document.getElementById("close-form-add-list");
 const formAddList = document.getElementById("form-add-list");
+const contenedorListas = document.getElementById("container-flujo");
+const firtList = document.getElementsByClassName("colum-list");
 
 const DISPLAY_FLEX = "flex";
 const DISPLAY_NONE = "none";
@@ -46,8 +48,48 @@ formAddList.addEventListener("submit", (e) => {
   });
   const dataForm = Object.fromEntries(formData);
   const dato = JSON.stringify(dataForm);
- 
+  changeFormAddlistVisibility();
+  addList(data);
 });
+
+function addList(data) {
+  const newList = document.createElement("div");
+  const newListDivHead = document.createElement("div");
+  const newListDivHeadTitle = document.createElement("div");
+  const newListHeadTitle = document.createElement("label");
+  newListHeadTitle.textContent = data["newListName"];
+  newListHeadTitle.classList.add("title-list");
+  newListHeadTitle.contentEditable = true;
+  newListDivHeadTitle.appendChild(newListHeadTitle);
+  const svgHtml = `
+  <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="menu-list"
+  >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+      <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+      <path d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+  </svg>
+  `;
+  newListDivHeadTitle.innerHTML += svgHtml;
+  newListDivHeadTitle.classList.add("container-title-list");
+  newListDivHead.appendChild(newListDivHeadTitle);
+  newListDivHead.classList.add("containt-list");
+  newList.classList.add("colum-list");
+  newList.classList.add("colum-list-tareas");
+  newList.appendChild(newListDivHead);
+
+  contenedorListas.insertBefore(newList, firtList[firtList.length - 1]);
+}
 
 // const modal = document.getElementById("modal-add-tarea");
 // const closeModalAddTarea = document.getElementById("closeModalBtn");
