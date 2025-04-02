bc7722e3471541489e2f3a2f9477a1e4207659ba
Isra Guitar <israelflores408@gmail.com>
2025-04-02 17:53:13 -0500

    Almacenamiento de las listas de tareas

--- scri.js
+++ scri.js
@@ -8,6 +8,8 @@ const formAddList = document.getElementById("form-add-list");
 const contenedorListas = document.getElementById("container-flujo");
 const firtList = document.getElementsByClassName("colum-list");
 
+const resetLocal = $("#reset-local");
+
 const addTareasButton = document.querySelectorAll(".container-add-tarea");
 const containAddTareasForm = document.querySelectorAll(
   ".contain-form-add-tareas"
@@ -19,9 +21,7 @@ const addTareaForm = document.querySelectorAll(".form-add-tarea");
 const DISPLAY_FLEX = "flex";
 const DISPLAY_NONE = "none";
 let isFormAddListVisible = false;
-let isFormAddTareaVisible = false;
 
-console.log(Date.now());
 function changeFormAddlistVisibility() {
   if (isFormAddListVisible) {
     containformAddList.style.display = DISPLAY_NONE;
@@ -30,6 +30,7 @@ function changeFormAddlistVisibility() {
   } else {
     addTareaButton.style.display = DISPLAY_NONE;
     containformAddList.style.display = DISPLAY_FLEX;
+    containformAddList.querySelector("#newListName").focus();
   }
   isFormAddListVisible = !isFormAddListVisible;
 }
@@ -61,6 +62,7 @@ window.addEventListener("click", (event) => {
   if (!containerFormTareaTarget && !containerAddTareaTarget) {
     allFormContainers.forEach((formContainer) => {
       formContainer.style.display = DISPLAY_NONE;
+      formContainer.querySelector(".form-add-tarea").reset();
     });
     allAddButtons.forEach((button) => {
       button.style.display = DISPLAY_FLEX;
@@ -74,6 +76,7 @@ window.addEventListener("click", (event) => {
     allFormContainers.forEach((formContainer) => {
       if (formContainer != containerFormTareaTarget) {
         formContainer.style.display = DISPLAY_NONE;
+        formContainer.querySelector(".form-add-tarea").reset();
       }
     });
     allAddButtons.forEach((button) => {
@@ -91,6 +94,7 @@ window.addEventListener("click", (event) => {
     allFormContainers.forEach((formContainer) => {
       if (formContainer != formContainerTemp) {
         formContainer.style.display = DISPLAY_NONE;
+        formContainer.querySelector(".form-add-tarea").reset();
       }
     });
     allAddButtons.forEach((button) => {
@@ -208,7 +212,278 @@ function addList(data) {
       <button type="submit" class="button-submit-accept">
         Añadir tarea
       </button>
-      <button class="button-without-background" id="close-form-add-tarea">
+      <button type="button" class="button-without-background" id="close-form-add-tarea">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+          <path d="M18 6l-12 12" />
+          <path d="M6 6l12 12" />
+        </svg>
+      </button>
+    </div>
+  </form>
+  `;
+
+  const contenedorFormAddTareasDivList = createElement(
+    "div",
+    {
+      class: "contain-adds contain-form-add-tareas",
+      id: "contain-form-add-tarea",
+    },
+    [formHtmlAddTareaList]
+  );
+
+  const containList = createElement("div", { class: "containt-list" }, [
+    headDivList,
+    contenedorTareasDivList,
+    contenedorAddTareasDivList,
+    contenedorFormAddTareasDivList,
+  ]);
+
+  const newList = createElement(
+    "div",
+    { class: "colum-list colum-list-tareas" },
+    [containList]
+  );
+
+  contenedorListas.insertBefore(newList, firtList[firtList.length - 1]);
+  setupDynamicEvents();
+  setInfoToStorage();
+}
+
+function setupDynamicEvents() {
+  document.removeEventListener("click", handleClickEvents);
+  document.removeEventListener("submit", handleSubmitEvents);
+  document.removeEventListener("change", handleChangeEvents);
+
+  document.addEventListener("click", handleClickEvents);
+  document.addEventListener("submit", handleSubmitEvents);
+  document.addEventListener("change", handleChangeEvents);
+}
+
+function handleChangeEvents(event) {
+  const checkbox = event.target.closest(".checkbox");
+
+  if (checkbox) {
+    setInfoToStorage();
+  }
+}
+
+function handleClickEvents(event) {
+  const button = event.target.closest(".container-add-tarea");
+  const closeButton = event.target.closest("#close-form-add-tarea");
+
+  if (button) {
+    const formContainer = button.nextElementSibling;
+    button.style.display = DISPLAY_NONE;
+    formContainer.style.display = DISPLAY_FLEX;
+    formContainer.querySelector("input").focus();
+  }
+
+  if (closeButton) {
+    const formContainer = closeButton.closest(".contain-form-add-tareas");
+    const addButton = formContainer.previousElementSibling;
+    formContainer.style.display = DISPLAY_NONE;
+    addButton.style.display = DISPLAY_FLEX;
+
+    formContainer.querySelector(".form-add-tarea").reset();
+  }
+}
+
+function handleSubmitEvents(event) {
+  if (event.target.classList.contains("form-add-tarea")) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const formObject = Object.fromEntries(formData.entries());
+
+    if (formObject["newTareaName"] != "") {
+      const containtList = event.target.closest(".containt-list");
+      const containTareasTemp = containtList.querySelector(
+        ".container-tareas-list"
+      );
+
+      const formContainer = event.target.closest(".contain-form-add-tareas");
+      const addButton = formContainer.previousElementSibling;
+
+      addTarea(formObject["newTareaName"], containTareasTemp);
+      event.target.reset();
+    } else {
+      console.log("presiono x input vacio pero hizo submit");
+    }
+  }
+}
+
+function addTarea(data, contain) {
+  const titleTarea = createElement("label", { class: "title-tarea" }, [data]);
+  const checkboxTarea = createElement(
+    "input",
+    { class: "checkbox", type: "checkbox" },
+    []
+  );
+  const containtCheckTarea = createElement(
+    "label",
+    { class: "custom-checkbox" },
+    [checkboxTarea]
+  );
+
+  const containtTarea = createElement("div", { class: "containt-tarea" }, [
+    containtCheckTarea,
+    titleTarea,
+  ]);
+
+  contain.appendChild(containtTarea);
+  setupDynamicEvents();
+  setInfoToStorage();
+}
+
+function setInfoToStorage() {
+  const allList = document.querySelectorAll(".containt-list");
+  let listas = [];
+  Array.from(allList).forEach((list, indexList) => {
+    const titleElement = list.querySelector(".title-list");
+
+    const allTareas = list.querySelectorAll(".containt-tarea");
+    let tareas = [];
+    Array.from(allTareas).forEach((tarea, indexTarea) => {
+      const checkElement = tarea.querySelector(".checkbox");
+      const titleTareaElement = tarea.querySelector(".title-tarea");
+      const tareatmp = {
+        checked: checkElement.checked,
+        title: titleTareaElement.textContent,
+      };
+      tareas.push(tareatmp);
+    });
+    const listTmp = {
+      title: titleElement.textContent,
+      tareas: tareas,
+    };
+    listas.push(listTmp);
+  });
+  localStorage.setItem("tareas", JSON.stringify(listas));
+}
+
+function getInfoToStorage() {
+  const listas = JSON.parse(localStorage.getItem("tareas"));
+  if (listas) {
+    listas.forEach((list, indexList) => {
+      loadList(list.title, list.tareas);
+    });
+  }
+}
+
+function loadList(listTitle, listTareas) {
+  const titleList = createElement(
+    "label",
+    { class: "title-list", contenteditable: "true" },
+    [listTitle]
+  );
+
+  const svgHtmlMenuList = `
+  <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="menu-list"
+  >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+      <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+      <path d="M19 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+  </svg>
+  `;
+
+  const headDivList = createElement("div", { class: "container-title-list" }, [
+    titleList,
+    svgHtmlMenuList,
+  ]);
+
+  const contenedorTareasDivList = createElement("div", {
+    class: "container-tareas-list",
+  });
+
+  if (listTareas) {
+    listTareas.forEach((tarea, indexTarea) => {
+      const titleTarea = createElement("label", { class: "title-tarea" }, [
+        tarea.title,
+      ]);
+      const checkboxTarea = createElement(
+        "input",
+        { class: "checkbox", type: "checkbox" },
+        []
+      );
+      checkboxTarea.checked = tarea.checked;
+      const containtCheckTarea = createElement(
+        "label",
+        { class: "custom-checkbox" },
+        [checkboxTarea]
+      );
+
+      const containtTarea = createElement("div", { class: "containt-tarea" }, [
+        containtCheckTarea,
+        titleTarea,
+      ]);
+
+      contenedorTareasDivList.appendChild(containtTarea);
+    });
+  }
+
+  const labelHtmlAddTareaList = `
+    <label>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M12 5l0 14" />
+        <path d="M5 12l14 0" />
+      </svg>
+      Añadir nueva tarea
+    </label>
+  `;
+  const contenedorAddTareasDivList = createElement(
+    "div",
+    { class: "container-add-tarea" },
+    [labelHtmlAddTareaList]
+  );
+
+  const formHtmlAddTareaList = `
+  <form id="form-add-tarea" class="form-add-tarea">
+    <input
+      type="text"
+      class="input-max-width"
+      placeholder="Ingrese el nombre de la tarea"
+      name="newTareaName"
+      id="newTareaName"
+      autocomplete="off"
+    />
+    <div class="content-button-adds">
+      <button type="submit" class="button-submit-accept">
+        Añadir tarea
+      </button>
+      <button type="button" class="button-without-background" id="close-form-add-tarea">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="24"
@@ -255,59 +530,16 @@ function addList(data) {
   setupDynamicEvents();
 }
 
-function setupDynamicEvents() {
-  document.removeEventListener("click", handleClickEvents);
-  document.removeEventListener("submit", handleSubmitEvents);
+resetLocal.addEventListener("click", () => {
+  localStorage.clear();
+  const allList = document.querySelectorAll(".colum-list-tareas");
 
-  document.addEventListener("click", handleClickEvents);
-  document.addEventListener("submit", handleSubmitEvents);
-}
+  allList.forEach((element) => {
+    element.remove();
+  });
+});
 
-function handleClickEvents(event) {
-  const button = event.target.closest(".container-add-tarea");
-  const closeButton = event.target.closest("#close-form-add-tarea");
-
-  if (button) {
-    const formContainer = button.nextElementSibling;
-    button.style.display = DISPLAY_NONE;
-    formContainer.style.display = DISPLAY_FLEX;
-    isFormAddTareaVisible = true;
-  }
-
-  if (closeButton) {
-    const formContainer = closeButton.closest(".contain-form-add-tareas");
-    const addButton = formContainer.previousElementSibling;
-    formContainer.style.display = DISPLAY_NONE;
-    addButton.style.display = DISPLAY_FLEX;
-    isFormAddTareaVisible = false;
-  }
-}
-
-function handleSubmitEvents(event) {
-  if (event.target.classList.contains("form-add-tarea")) {
-    event.preventDefault();
-
-    const formData = new FormData(event.target);
-    const formObject = Object.fromEntries(formData.entries());
-
-    const containtList = event.target.closest(".containt-list");
-    const containTareasTemp = containtList.querySelector(
-      ".container-tareas-list"
-    );
-
-    const formContainer = event.target.closest(".contain-form-add-tareas");
-    const addButton = formContainer.previousElementSibling;
-
-    addTarea(formObject["newTareaName"], containTareasTemp);
-    event.target.reset();
-  }
-}
-
-setupDynamicEvents();
-
-function addTarea(data, contain) {
-  contain.appendChild(createElement("div", {}, [data]));
-}
+getInfoToStorage();
 
 // const modal = document.getElementById("modal-add-tarea");
 // const closeModalAddTarea = document.getElementById("closeModalBtn");
